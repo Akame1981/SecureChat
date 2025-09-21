257bdd65e4f2703633d40f8dee88e5dba6eabf04
Akame1981 <omcompjoni1981@gmail.com>
2025-09-21 16:04:30 +0300

    Better display for names/ fixed

--- utils/network.py
+++ utils/network.py
@@ -1,7 +1,7 @@
 import requests
 from utils.crypto import encrypt_message, decrypt_message, sign_message
 
-USE_PUBLIC_SERVER = True  # Set False for localhost
+USE_PUBLIC_SERVER = False  # Set False for localhost
 
 if USE_PUBLIC_SERVER:
     SERVER_URL = "http://34.61.34.132:8000" # That is the official public server. Use it or your own.
@@ -9,21 +9,23 @@ else:
     SERVER_URL = "http://127.0.0.1:8000"
 
 
-def send_message(to_pub: str, from_sign_pub: str, text: str, signing_key):
+
+def send_message(to_pub: str, signing_pub: str, text: str, signing_key, enc_pub: str):
     """
     Send a message to the server.
     - to_pub: recipient's encryption public key (hex)
-    - from_sign_pub: your signing public key (hex)
+    - signing_pub: your signing public key (hex)
     - text: plaintext message
     - signing_key: your SigningKey object
+    - enc_pub: your encryption public key (hex) for display
     """
     try:
         encrypted_b64 = encrypt_message(text, to_pub)
         signature_b64 = sign_message(encrypted_b64, signing_key)
-
         payload = {
             "to": to_pub,
-            "from_": from_sign_pub,  
+            "from_": signing_pub,   # server verifies signature with this
+            "enc_pub": enc_pub,      # this is for chat display
             "message": encrypted_b64,
             "signature": signature_b64
         }
@@ -49,7 +51,8 @@ def fetch_messages(my_pub_hex: str, private_key):
             msgs = []
             for msg in inbox:
                 msgs.append({
-                    "from": msg["from"],       
+                    "from_sign": msg["from"],     # signing key (for verification)
+                    "from_enc": msg["enc_pub"],   # encryption key (for display)
                     "message": msg["message"],
                     "signature": msg.get("signature")
                 })
