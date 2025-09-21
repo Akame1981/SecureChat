322e47f303e66137634664b42d03834f3add17c6
Akame1981 <fartexofficial@gmail.com>
2025-09-21 16:56:38 +0300

    HTTPS Support in Networking

--- utils/network.py
+++ utils/network.py
@@ -1,12 +1,14 @@
 import requests
 from utils.crypto import encrypt_message, decrypt_message, sign_message
 
-USE_PUBLIC_SERVER = True  # Set False for localhost
+USE_PUBLIC_SERVER = True  # Set False for localhost / You wanna host your own server
 
 if USE_PUBLIC_SERVER:
-    SERVER_URL = "https://34.61.34.132:8000" # That is the official public server. Use it or your own.
+    SERVER_URL = "https://34.61.34.132:8000"  # Official Public Server 
+    SERVER_CERT = "utils/server-cert.pem"     
 else:
     SERVER_URL = "http://127.0.0.1:8000"
+    SERVER_CERT = None  # No cert for HTTP
 
 
 
@@ -24,13 +26,13 @@ def send_message(to_pub: str, signing_pub: str, text: str, signing_key, enc_pub:
         signature_b64 = sign_message(encrypted_b64, signing_key)
         payload = {
             "to": to_pub,
-            "from_": signing_pub,   # server verifies signature with this
-            "enc_pub": enc_pub,      # this is for chat display
+            "from_": signing_pub,
+            "enc_pub": enc_pub,
             "message": encrypted_b64,
             "signature": signature_b64
         }
 
-        r = requests.post(f"{SERVER_URL}/send", json=payload)
+        r = requests.post(f"{SERVER_URL}/send", json=payload, verify=SERVER_CERT)
         if not r.ok:
             print("Send Error:", r.status_code, r.text)
         return r.ok
@@ -45,14 +47,14 @@ def fetch_messages(my_pub_hex: str, private_key):
     Fetch messages addressed to your encryption public key (my_pub_hex)
     """
     try:
-        r = requests.get(f"{SERVER_URL}/inbox/{my_pub_hex}")
+        r = requests.get(f"{SERVER_URL}/inbox/{my_pub_hex}", verify=SERVER_CERT)
         if r.ok:
             inbox = r.json().get("messages", [])
             msgs = []
             for msg in inbox:
                 msgs.append({
-                    "from_sign": msg["from"],     # signing key (for verification)
-                    "from_enc": msg["enc_pub"],   # encryption key (for display)
+                    "from_sign": msg["from"],
+                    "from_enc": msg["enc_pub"],
                     "message": msg["message"],
                     "signature": msg.get("signature")
                 })
